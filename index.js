5b4e9fb374973e415b6b78687c3aff292acf5689
cwlowder <clowder2@illinois.edu>
2018-07-21 10:45:37 -0700

    NEXT

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require('express')
 var app = express()
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+	client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+		if (err) throw err;
+		for (let row of res.rows) {
+			console.log(JSON.stringify(row));
+		}
+		response.send('TEST');
+	});
 })
 
 app.listen(app.get('port'), function() {
